ccb8336098e29cb02cb57873d182f61e8c4a4808
Pengfei Han <hanpfei@gmail.com>
2016-08-31 15:49:23 +0800

    Add two tools.
    Change-Id: I59b5646b6103436848dd29318243f31bb7226640
    Signed-off-by: Pengfei Han <hanpfei@gmail.com>

--- CodeScaleCalculator.py
+++ CodeScaleCalculator.py
@@ -3,7 +3,6 @@ import sys;
 import string;
 
 if __name__ == "__main__":
-    print 'Hello, world.';
     dirpath = sys.argv[1];
     total_line_num = 0;
     filelist = []
@@ -19,7 +18,8 @@ if __name__ == "__main__":
                 sub_file_path = file + os.path.sep + subFile
                 filelist.append(sub_file_path)
         elif os.path.isfile(str(file)):
-            if (str(file).endswith("java") or str(file).endswith("cpp") or str(file).endswith(".h") or str(file).endswith(".c")):
+            if (str(file).endswith("java") or str(file).endswith("cpp") or str(file).endswith(".h") or str(file).endswith(".c")
+                or str(file).endswith(".cc")):
                 if (file.find("R.java") != -1):
                     continue
                 if (file.find("BuildConfig.java") != -1):
@@ -33,6 +33,14 @@ if __name__ == "__main__":
                 if (file.find("ortp") != -1):
                     continue
                 if (file.find("poco") != -1 or file.find("Poco") != -1) :
+                    continue
+                if (file.find("test") != -1):
+                    continue
+                if (file.find("tools") != -1):
+                    continue
+                if (file.find("ftp") != -1):
+                    continue
+                if (file.find("websockets") != -1):
                     continue
                 fp = open(file, "r+")
                 linenum = len(fp.readlines())
---
+++ PerfDataAnalyzer.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+
+import json
+import os
+import re
+import sys
+
+def usage():
+    basename = os.path.basename(sys.argv[0])
+    print str(basename) + " [test_data_file_path]"
+    exit(1)
+
+
+def parse_statistics_item(statistics_json_str_item, total_statistics):
+    statistics_json_item = json.loads(statistics_json_str_item)
+    libname = statistics_json_item["netlib"]
+    url = statistics_json_item["url"]
+    receivedBytes = statistics_json_item["receivedBytes"]
+    requestTime = statistics_json_item["time"]
+
+    if not url in total_statistics:
+        total_statistics[url] = {}
+    if not libname in total_statistics[url]:
+        total_statistics[url][libname] = {}
+
+    if receivedBytes > 0 and requestTime < 10000:
+        if not "totalTime" in total_statistics[url][libname]:
+            total_statistics[url][libname]["totalTime"] = requestTime
+        else:
+            total_statistics[url][libname]["totalTime"] += requestTime
+
+        if not "totalItemNum" in total_statistics[url][libname]:
+            total_statistics[url][libname]["totalItemNum"] = 1
+        else:
+            total_statistics[url][libname]["totalItemNum"] += 1
+
+        if not "libname" in total_statistics[url][libname]:
+            total_statistics[url][libname]["libname"] = libname
+
+        if not "url" in total_statistics[url][libname]:
+            total_statistics[url][libname]["url"] = url
+
+        total_statistics[url][libname]["length"] = receivedBytes
+
+
+def compute_average_time(total_statistics):
+    urls = total_statistics.keys()
+    for url in urls:
+        libnames = total_statistics[url].keys()
+        for lib in libnames:
+            total_statistics[url][lib]["averageTime"] = total_statistics[url][lib]["totalTime"] / total_statistics[url][lib]["totalItemNum"]
+
+
+def print_statistics(total_statistics):
+    urls = total_statistics.keys()
+    urls = sorted(urls)
+    libnames = total_statistics[urls[0]].keys()
+    libnames = sorted(libnames)
+    print ("%-50s%-20s%-20s%-20s%-20s%-20s" % ("Url", "data size(bytes)", libnames[0] + "(ms)",
+                                                       libnames[1] + "(ms)", libnames[2] + "(ms)",
+                                                       libnames[3] + "(ms)"))
+
+    for url in urls:
+        libnames = total_statistics[url].keys()
+        libnames = sorted(libnames)
+        print ("%-50s%-20d%-20d%-20d%-20d%-20d" % (url, total_statistics[url][libnames[0]]["length"],
+                                                   total_statistics[url][libnames[0]]["averageTime"],
+                                                   total_statistics[url][libnames[1]]["averageTime"],
+                                                   total_statistics[url][libnames[2]]["averageTime"],
+                                                   total_statistics[url][libnames[3]]["averageTime"]))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        usage()
+    perf_test_data_path = sys.argv[1]
+
+    if not os.path.exists(perf_test_data_path):
+        usage()
+
+    fp = open(perf_test_data_path, 'r+');
+    all_test_lines = fp.readlines()
+
+    total_statistics = {}
+    pattern = re.compile(r".+statistics=(\{.+\}$)")
+    for line in all_test_lines:
+        line = line.strip()
+        matcher = pattern.match(line)
+        if matcher:
+            statisticsJsonStrItem = matcher.group(1)
+
+            parse_statistics_item(statisticsJsonStrItem, total_statistics)
+
+    compute_average_time(total_statistics)
+    print_statistics(total_statistics)
